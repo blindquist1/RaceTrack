0eab49fd4e92e5a39f35e2bf6416d016b17266b9
Brie Lindquist <blindquist1@my.wctc.edu>
2023-03-13 18:58:09 -0500

    Add project files.

---
+++ RaceTrack/Cars/Ferrari.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Ferrari : RaceCar
+    {
+        public Ferrari()
+        {
+            Name = "Ferrari";
+            TopSpeed = 200;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} stares down the Ford GT");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} is squealing it's tires!");
+            base.Brake();
+        }
+    }
+}
---
+++ RaceTrack/Drivers/AceStudent.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class AceStudent : Driver
+    {
+        public AceStudent(RaceCar car) : base(car)
+        {
+            Name = "Ace Student";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
